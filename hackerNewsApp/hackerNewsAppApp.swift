dd80f5082c75be1a3a6ec342d340359543501e2b
Rafael Freitas <rafaelborges1985@hotmail.com>
2020-10-15 21:54:16 -0300

    Initial Commit

---
+++ hackerNewsApp/hackerNewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  hackerNewsAppApp.swift
+//  hackerNewsApp
+//
+//  Created by Rafael Freitas on 15/10/20.
+//
+
+import SwiftUI
+
+@main
+struct hackerNewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
