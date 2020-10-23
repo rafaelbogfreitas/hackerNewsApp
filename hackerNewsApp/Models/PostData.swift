9188740d17a102783907b24bcc8610cfde23be30
Rafael Freitas <rafaelborges1985@hotmail.com>
2020-10-23 00:04:27 -0300

    Implement Codable data struct to parse API response

--- hackerNewsApp/Models/PostData.swift
+++ hackerNewsApp/Models/PostData.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+struct Results: Codable {
+    let hits:[Post]
+}
+
+struct Post: Codable, Identifiable {
+    var id: String {
+        return objectID
+    }
+    
+    let objectID: String
+    let points: Int
+    let title: String
+    let url: String?
+}
