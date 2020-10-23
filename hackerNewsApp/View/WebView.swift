824ee6e126e9981240aa02b78897f62312f44513
Rafael Freitas <rafaelborges1985@hotmail.com>
2020-10-23 00:07:36 -0300

    Create WebView struct with UIKit components with SwiftUI

--- hackerNewsApp/View/WebView.swift
+++ hackerNewsApp/View/WebView.swift
@@ -5,4 +5,24 @@
 //  Created by Rafael Freitas on 21/10/20.
 //
 
-import Foundation
+import SwiftUI
+import WebKit
+
+struct WebView: UIViewRepresentable {
+    
+    let urlString: String?
+    
+    func makeUIView(context: Context) -> WebView.UIViewType {
+        return WKWebView()
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        if let safeString = urlString {
+            if let url = URL(string: safeString) {
+                let request = URLRequest(url: url)
+                uiView.load(request)
+            }
+        }
+    }
+    
+}
